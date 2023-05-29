a20927272e99086ad46fa8a8f87832aa5a74d616
mSafa <safaktrcgl@gmail.com>
2023-05-29 19:33:46 +0300

    Engine Initialization

---
+++ PrimalEditor/GameProject/NewProject.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace PrimalEditor.GameProject
+{
+    public class ProjectTemplate
+    {
+        public string ProjectType { get; set; }
+        public string ProjectFile { get; set; }
+        public List<string> ProjectFolders { get; set; }
+    }
+
+    class NewProject : ViewModelBase
+    {
+        private readonly string _templatePath = @"..\..\PrimalEditor\ProjectTemplates";
+        private string _name = "NewProject";
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PrimalProject\";
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                if (_path != value)
+                {
+                    _path = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+
+        public NewProject()
+        {
+            try
+            {
+                string[] templates = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+
+}
