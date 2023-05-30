97e271713e32d184a23386aaf0fd701a459a562a
mSafa <safaktrcgl@gmail.com>
2023-05-31 01:11:02 +0300

    Implement create new project window

--- PrimalEditor/GameProject/NewProject.cs
+++ PrimalEditor/GameProject/NewProject.cs
@@ -1,59 +1,91 @@
-﻿using System;
+﻿using PrimalEditor.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace PrimalEditor.GameProject
 {
+    [DataContract]
     public class ProjectTemplate
     {
+        [DataMember]
         public string ProjectType { get; set; }
+        [DataMember]
         public string ProjectFile { get; set; }
+        [DataMember]
         public List<string> ProjectFolders { get; set; }
+
+        public string ProjectFilePath { get; set; }
+
+        public byte[] Icon { get; set; }
+        public string IconFilePath { get; set; }
+        
+        public byte[] Screenshot { get; set; }
+        public string ScreenshotFilePath { get; set; }
     }
 
     class NewProject : ViewModelBase
     {
         private readonly string _templatePath = @"..\..\PrimalEditor\ProjectTemplates";
-        private string _name = "NewProject";
-        public string Name
+        private string _projectName = "NewProject";
+        public string ProjectName
         {
-            get => _name;
+            get => _projectName;
             set
             {
-                if (_name != value)
+                if (_projectName != value)
                 {
-                    _name = value;
-                    OnPropertyChanged(nameof(Name));
+                    _projectName = value;
+                    OnPropertyChanged(nameof(ProjectName));
                 }
             }
         }
 
-        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PrimalProject\";
-        public string Path
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PrimalProject\";
+        public string ProjectPath
         {
-            get => _path;
+            get => _projectPath;
             set
             {
-                if (_path != value)
+                if (_projectPath != value)
                 {
-                    _path = value;
-                    OnPropertyChanged(nameof(Path));
+                    _projectPath = value;
+                    OnPropertyChanged(nameof(ProjectPath));
                 }
             }
         }
+
+        private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates { get; }
 
         public NewProject()
         {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
             try
             {
-                string[] templates = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                string[] templateFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templateFiles.Any());
+                foreach (string file in templateFiles)
+                {
+                    ProjectTemplate template = Serializer.FromFile<ProjectTemplate>(file);
+                    template.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
+                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "icon.png"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "screenshot.png"));
+                    template.Screenshot = File.ReadAllBytes(template.ScreenshotFilePath);
+
+                    _projectTemplates.Add(template);
+                }
             }
             catch (Exception ex)
             {
                 Debug.WriteLine(ex.Message);
-                throw;
+                //TODO: Proper Logging
             }
         }
     }
---
+++ PrimalEditor/Utilities/Serializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace PrimalEditor.Utilities
+{
+    public static class Serializer
+    {
+        public static void ToFile<T>(T instance, string path)
+        {
+            try
+            {
+                using var fileStream = new FileStream(path, FileMode.Create);
+                var serializer = new DataContractSerializer(typeof(T));
+                serializer.WriteObject(fileStream, instance);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                //TODO: Proper Logging
+            }
+        }
+
+        internal static T FromFile<T>(string path)
+        {
+            try
+            {
+                using var fileStream = new FileStream(path, FileMode.Open);
+                var serializer = new DataContractSerializer(typeof(T));
+                T instance = (T)serializer.ReadObject(fileStream);
+                return instance;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                //TODO: Proper Logging
+
+                return default(T);
+            }
+        }
+    }
+}
